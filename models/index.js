730b2aee25b36e6e768ccec957f763e7004483ec
Mitch Strange <mstrange382@gmail.com>
2021-10-21 17:27:07 -0500

    set up associations

--- models/index.js
+++ models/index.js
@@ -4,6 +4,13 @@ const Comment = require('./comment')
 // create individual files for your models and import them here
 
 // Setup Associations
+User.hasMany(Comment);
+Comment.belongsTo(User);
+
+User.hasMany(Idea);
+Idea.belongsTo(User);
+
+Idea.hasMany(Comment);
 
 module.exports = {
   User,
